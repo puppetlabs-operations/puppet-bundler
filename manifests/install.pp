6f4c84a711874a9776c1ee2a851f13af9cebbe64
Adrien Thebo <adrien@puppetlabs.com>
2012-05-31 14:26:29 -0700

    Add dox for bundler::install

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,3 +1,20 @@
+# = Define: bundler::install
+#
+# Perform a bundle install in a given directory. Wraps all of the
+# fiddly little configuration guts.
+#
+# == Example
+#
+#  # Deploy a sinatra app
+#  sinatra::app { 'appy':
+#    giturl   => 'git://your.git.server/your/git/remote',
+#  }
+#
+#  # And then run bundler
+#  bundler::install {'/opt/boardie':
+#    require => Sinatra::App['boardie'],
+#  }
+#
 define bundler::install($gem_bindir = '/var/lib/gems/1.8/bin') {
 
   require bundler
